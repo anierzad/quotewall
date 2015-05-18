c317bf2446150451105f612dce5ad1506b98e4b9
Adam Nierzad <a.nierzad@gmail.com>
2015-05-18 14:30:30 +0100

    Added interval so home screen data is refreshed periodically.

--- client/public/controllers/HomeQuotesController.js
+++ client/public/controllers/HomeQuotesController.js
@@ -2,14 +2,29 @@
 
 	var module = angular.module('QuoteWall');
 
-	module.controller('HomeQuotesController', ["$scope", "DataService", 
-		function($scope, DataService) {
+	module.controller('HomeQuotesController', ['$scope', '$interval', 'DataService', 
+		function($scope, $interval, DataService) {
 
-			DataService.getQuotes().then(
-				function(data) {
-					if(data.length) {
-						$scope.quotes = data;
-					}
-				});
+			$scope.quotes;
+
+			var getData = function() {
+				console.log("Getting data.");
+				DataService.getQuotes().then(
+					function(data) {
+						if(data.length) {
+							if(!$scope.quotes || $scope.quotes.length != data.length )
+
+							$scope.quotes = data;
+						}
+					});
+			};
+
+			getData();
+
+			var dataInterval = $interval(getData, 1000);
+			
+			$scope.$on('destroy', function() {
+				$interval.cancel(dataInterval);
+			});
 		}]);
 })();
